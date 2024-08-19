a92e57bb8d059965fb264f264f0820467146fc45
Prateek Das <prateek17das@gmail.com>
2024-08-19 13:40:15 +0600

    Remove Duplicate Skeletons

---
+++ app/issues/edit/page.tsx
@@ -0,0 +1,25 @@
+import prisma from "@/prisma/client";
+import dynamic from "next/dynamic";
+import { notFound } from "next/navigation";
+import React from "react";
+import IssueFormSkeletoton from "./[id]/loading";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeletoton />,
+});
+
+interface Props {
+  params: { id: string };
+}
+const EditIssuePage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) notFound();
+
+  return <IssueForm issue={issue} />;
+};
+
+export default EditIssuePage;
