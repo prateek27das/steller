947f5a5214049e0582863a9d51d6c26e195893e3
Prateek Das <prateek17das@gmail.com>
2024-09-23 22:02:03 +0600

    Refactor

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    typescript: {
+        ignoreBuildErrors: true, 
+    },
     async headers() {
         return[
             {
